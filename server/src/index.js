ac7fb769784433c8a6337b99f68e19db1ab2e1b7
bjarkt <bjarke.tobiesen@gmail.com>
2019-10-27 17:11:58 +0100

    Server: Add swagger documentation

--- server/src/index.js
+++ server/src/index.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
 
 const app = express();
 const expressWs = require("express-ws")(app);
 const sessionHandler = require("./routes/sessionHandler");
 
+const swaggerRest = YAML.load("docs/swagger.yaml");
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.ws("/session/:url", sessionHandler.wsHandler);
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerRest));
 
-app.post("/session", sessionHandler.createSession);
-app.get("/session/:url", sessionHandler.getSessionInfo);
+app.ws("/api/session/:url", sessionHandler.wsHandler);
+
+app.post("/api/session", sessionHandler.createSession);
+app.get("/api/session/:url", sessionHandler.getSessionInfo);
 
 app.listen(port, () => console.log(`Listening on ${port}`));
 
 /*
-Node, Express, REST, WebSocket
+Node, Express, REST, WebSocket, Swagger
 */
