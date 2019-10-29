1489da2a79c90e06d64d1eb662080fb02212835a
bjarkt <bjarke.tobiesen@gmail.com>
2019-10-29 11:08:53 +0100

    Server: add CORS

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
+const cors = require("cors");
 
 const app = express();
 const expressWs = require("express-ws")(app);
@@ -12,6 +13,7 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerRest));
 
